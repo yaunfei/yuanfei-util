a61ec588ec99924c39101631d6f0757e26e056d7
郑园飞 <zhengyuanfei@souche.com>
2021-12-03 23:37:24 +0800

    feat:添加导出方式

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
+/**
+ * 1.直接导出每个方法
+ * 如：amount的 formatterMoney
+ * 引用方式（要花括号）：
+ * import { formatterMoney } from 'yuanfei-util'
+ * 使用：
+ * formatterMoney()
+ */
 export * from './amount';
+
+
+/**
+ * 2.指定默认导出
+ * 如：amount导出被定义一个别名Util
+ * 引用方式（不要花括号）：
+ * import Util from 'yuanfei-util'
+ * 使用:
+ * Util.Amount.formatterMoney()
+ */
+import * as Amount from './amount';
+export default {
+  Amount,
+}
+
+/**
+ * 3.正常导出模块
+ * 如：amount导出
+ * 引用方式（要花括号）：
+ * import { Amount } from 'yuanfei-util'
+ * 使用:
+ * Amount.formatterMoney()
+ */
+export {
+  Amount,
+}
+
