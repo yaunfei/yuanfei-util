08af8b8857cf3c289e895795e9196fa3ba46ebcf
郑园飞 <zhengyuanfei@souche.com>
2021-12-09 16:37:19 +0800

    fix:更换decimal包

--- types/cache.d.ts
+++ types/cache.d.ts
@@ -4,9 +4,9 @@ declare function setCookie(
   expiresDay?: string,
   domian?: string,
   path?: string
-);
+): void;
 
 declare function getCookie(key: string): string;
-declare function setData(key: string, value: any);
+declare function setData(key: string, value: any): void;
 declare function getData(key: string): any;
 export { setCookie, getCookie, setData, getData };
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,1 +1,10 @@
-export * from './amount';
+export * from "./amount";
+export * from "./base";
+export * from "./date";
+export * from "./cache";
+
+import * as Amount from "./amount";
+import * as Base from "./base";
+import * as Date from "./date";
+import * as Cache from "./cache";
+export { Amount, Base, Date, Cache };
